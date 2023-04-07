3bcb48007291bf380e5874687e0738ac4984f68d
duuree17 <duuren0315@gmail.com>
2023-04-08 02:25:30 +0800

    kod tsegtselsen

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,55 +1,47 @@
-const { User } = require("../Models/user.model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const { User } = require('../Models/user.model')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 exports.signupUser = async (req, res) => {
-  const { username, password, role } = req.body;
-  const checkUsername = await User.findOne({ username });
-  if (!checkUsername) {
-    if (username && password) {
-      try {
-        const encryptedPassword = await bcrypt.hash(password, 10);
-        const user = await new User({
-          username,
-          password: encryptedPassword,
-          role,
-        }).save();
-        const token = jwt.sign({ user }, process.env.JWT_SECRET, {
-          expiresIn: "24h",
-        });
-        res.send(token);
-      } catch (err) {
-        res.status(403);
-      }
-    }
-  } else {
-    res.status(401).send("Username is already in use");
+  const { username, password } = req.body
+  const checkUsername = await User.findOne({ username }, { password: 0 })
+  try {
+    if (checkUsername) return res.status(401).send('Username is already in use')
+    if (!username || !password)
+      return res.status(401).send('Username & Password not found!')
+
+    const encryptedPassword = await bcrypt.hash(password, 10)
+    const user = await new User({
+      username,
+      password: encryptedPassword,
+      role: req.body.role || 'user',
+    }).save()
+    const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+      expiresIn: '24h',
+    })
+    res.send(token)
+  } catch (err) {
+    res.status(403)
   }
-};
+}
 
 exports.loginUser = async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  if (user) {
-    console.log(`Entered accounts user : ${user}`);
-    try {
-      const isEqual = await bcrypt.compare(password, user.password);
-      if (isEqual) {
-        const token = jwt.sign({ user }, process.env.JWT_SECRET, {
-          expiresIn: "24h",
-        });
-        res.send(token);
-      } else {
-        res.status(401).send("username or password wrong");
-      }
-    } catch (err) {
-      res.status(403);
-    }
-  } else {
-    res.status(404).send("No account found");
+  const { username, password } = req.body
+  const user = await User.findOne({ username })
+  if (!user) return res.status(404).send('No account found')
+
+  try {
+    const isEqual = await bcrypt.compare(password, user.password)
+    if (!isEqual) return res.status(401).send('username or password wrong')
+    const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+      expiresIn: '24h',
+    })
+    res.send(token)
+  } catch (err) {
+    res.status(403)
   }
-};
+}
 
 exports.VerifyToken = async (req, res) => {
   if (req.headers.authorization) {
@@ -59,14 +51,14 @@ exports.VerifyToken = async (req, res) => {
         process.env.JWT_SECRET,
         (error, item) => {
           if (!error) {
-            res.send(item.user);
+            res.send(item.user)
           }
-        }
-      );
+        },
+      )
     } catch (error) {
-      res.status(401);
+      res.status(401)
     }
   } else {
-    res.status(404).send("Authentication required");
+    res.status(404).send('Authentication required')
   }
-};
+}
