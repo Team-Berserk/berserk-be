6444bf0b99363e91ba00b6d649318c5395e62856
duuree17 <duuren0315@gmail.com>
2023-05-01 17:39:43 +0800

    middlewares added?

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,50 +1,49 @@
-const { User } = require("../Models/user.model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const { User } = require('../Models/user.model')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 exports.signupUser = async (req, res) => {
-  const { username, password } = req.body;
-  const checkUsername = await User.findOne({ username });
+  const { username, password } = req.body
+  const checkUsername = await User.findOne({ username })
   try {
-    if (checkUsername)
-      return res.status(401).send("Username is already in use");
+    if (checkUsername) return res.status(401).send('Username is already in use')
     if (!username || !password)
-      return res.status(401).send("Username & Password required!");
+      return res.status(401).send('Username & Password required!')
 
-    const encryptedPassword = await bcrypt.hash(password, 10);
+    const encryptedPassword = await bcrypt.hash(password, 10)
     const user = await new User({
       username,
       password: encryptedPassword,
-    }).save();
+    }).save()
     const token = jwt.sign({ user }, process.env.JWT_SECRET, {
-      expiresIn: "24h",
-    });
-    res.send(token);
+      expiresIn: '24h',
+    })
+    res.send(token)
   } catch (err) {
-    res.status(403);
+    res.status(403)
   }
-};
+}
 
 exports.loginUser = async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  if (!user) return res.status(404).send("No account found");
+  const { username, password } = req.body
+  const user = await User.findOne({ username })
+  if (!user) return res.status(404).send('No account found')
 
   try {
-    const isEqual = await bcrypt.compare(password, user.password);
-    if (!isEqual) return res.status(401).send("username or password wrong");
+    const isEqual = await bcrypt.compare(password, user.password)
+    if (!isEqual) return res.status(403).send('username or password wrong')
     const token = jwt.sign({ user }, process.env.JWT_SECRET, {
-      expiresIn: "24h",
-    });
-    res.send(token);
+      expiresIn: '24h',
+    })
+    res.send(token)
   } catch (err) {
-    res.status(403);
+    res.status(403)
   }
-};
+}
 
 exports.VerifyToken = async (req, res) => {
-  if (!req.headers.authorization) return res.send("Token Required");
+  if (!req.headers.authorization) return res.send('Token Required')
 
   try {
     jwt.verify(
@@ -52,13 +51,13 @@ exports.VerifyToken = async (req, res) => {
       process.env.JWT_SECRET,
       async (error, item) => {
         if (!error) {
-          const user = await User.findById(item.user._id).populate("requests");
+          const user = await User.findById(item.user._id).populate('requests')
           // console.log(user);
-          res.send(user);
+          res.send(user)
         }
-      }
-    );
+      },
+    )
   } catch (error) {
-    res.status(401);
+    res.status(401)
   }
-};
+}
--- config/Middlewares/auth.middlewares.js
+++ config/Middlewares/auth.middlewares.js
@@ -1,46 +1,46 @@
-const jwt = require("jsonwebtoken");
-const User = require("../../Models/user.model");
+const jwt = require('jsonwebtoken')
+const User = require('../../Models/user.model')
 
 const isAdmin = async (req, res, next) => {
   try {
-    const token = req.headers.authorization;
-    if (!token) return res.status(404).send({ message: "Token required!" });
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const token = req.headers.authorization
+    if (!token) return res.status(404).send({ message: 'Token required!' })
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    const user = decoded.user;
+    const user = decoded.user
 
     if (!user) {
-      throw new Error();
+      throw new Error()
     }
 
-    if (!user.role.includes("admin")) {
-      throw new Error("You are not authorized to perform this action");
+    if (!user.role.includes('admin')) {
+      throw new Error('You are not authorized to perform this action')
     }
 
-    req.user = user;
-    req.token = token;
-    next();
+    req.user = user
+    req.token = token
+    next()
   } catch (error) {
-    res.status(401).send({ error: "Please authenticate." });
+    res.status(402).send({ error: 'Please authenticate.' })
   }
-};
+}
 const isLoggedIn = async (req, res, next) => {
   try {
-    const token = req.headers.authorization;
-    if (!token) return res.status(404).send({ message: "Token required!" });
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const token = req.headers.authorization
+    if (!token) return res.status(404).send({ message: 'Token required!' })
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    const user = decoded.user;
+    const user = decoded.user
 
     if (!user) {
-      throw new Error();
+      throw new Error()
     }
-    req.user = user;
-    req.token = token;
-    next();
+    req.user = user
+    req.token = token
+    next()
   } catch (error) {
-    res.status(401).send({ error: "Please authenticate." });
+    res.status(401).send({ error: 'Please authenticate.' })
   }
-};
+}
 
-module.exports = { isAdmin, isLoggedIn };
+module.exports = { isAdmin, isLoggedIn }
